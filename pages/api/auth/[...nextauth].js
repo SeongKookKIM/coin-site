b7bb3a702795a3a29fa52afca14f81edf8563d9a
SeongkookKIM <ksk8646@naver.com>
2023-06-28 17:57:03 +0900

    next-auth 수정

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -7,16 +7,12 @@ import bcrypt from "bcrypt";
 export const authOptions = {
   providers: [
     CredentialsProvider({
-      //1. 로그인페이지 폼 자동생성해주는 코드
       name: "credentials",
       credentials: {
         id: { label: "Id", type: "text" },
         password: { label: "password", type: "password" },
       },
 
-      //2. 로그인요청시 실행되는코드
-      //직접 DB에서 아이디,비번 비교하고
-      //아이디,비번 맞으면 return 결과, 틀리면 return null 해야함
       async authorize(credentials) {
         let db = (await connectDB).db("coin");
         let user = await db.collection("user").findOne({ id: credentials.id });
@@ -37,38 +33,33 @@ export const authOptions = {
     }),
   ],
 
-  //3. jwt 써놔야 잘됩니다 + jwt 만료일설정
   session: {
     strategy: "jwt",
-    maxAge: 30 * 24 * 60 * 60, //30일
+    maxAge: 30 * 24 * 60 * 60,
   },
 
   callbacks: {
-    //4. jwt 만들 때 실행되는 코드
-    //user변수는 DB의 유저정보담겨있고 token.user에 뭐 저장하면 jwt에 들어갑니다.
     jwt: async ({ token, user }) => {
       if (user) {
         token.user = {};
         token.user.name = user.name;
+        token.user.id = user.id;
         token.user.email = user.email;
-        token.user.role = user.role;
       }
       return token;
     },
-    //5. 유저 세션이 조회될 때 마다 실행되는 코드
     session: async ({ session, token }) => {
       session.user = token.user;
       return session;
     },
   },
 
-  // @@@pages추가
   pages: {
     signIn: "/login",
   },
 
-  // secret: "qwer1234",
-  secret: process.env.NEXTAUTH_SECRET,
+  secret: "qwer1234",
+  // secret: process.env.NEXTAUTH_SECRET,
   //@npm install @next-auth/mongodb-adapter
   adapter: MongoDBAdapter(connectDB),
 };
